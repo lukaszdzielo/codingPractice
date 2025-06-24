fb2287300f1122206fe031dbca04d1f1792ff420
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2025-06-24 21:32:37 +0200

    update exercise 1

--- javascript-the-complete-guide-2020-beginner-advanced/01-calculator/scripts/app.js
+++ javascript-the-complete-guide-2020-beginner-advanced/01-calculator/scripts/app.js
@@ -1,6 +1,53 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-currentResult = currentResult + 10;
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
 
-outputResult(currentResult);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  const logEntry = {
+    operation: 'ADD',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);

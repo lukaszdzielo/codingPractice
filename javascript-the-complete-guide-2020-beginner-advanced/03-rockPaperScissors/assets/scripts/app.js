b6659cca37ed52c3d2bc5a6f10b6b72382dba5f9
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2025-07-14 20:24:49 +0200

    add: third exercise

---
+++ javascript-the-complete-guide-2020-beginner-advanced/03-rockPaperScissors/assets/scripts/app.js
@@ -0,0 +1,62 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_PLAYER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER WINS";
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+    const selection = prompt(
+        `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
+        ""
+    ).toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert(`Invalid choice! We chose ${ROCK} for you.`);
+        return DEFAULT_PLAYER_CHOICE;
+    }
+    return selection;
+};
+
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+const getWinner = function (cChoice, pChoice) {
+    if (cChoice === pChoice) {
+        return "It's a draw!";
+    } else if (
+        (cChoice === ROCK && pChoice === SCISSORS) ||
+        (cChoice === PAPER && pChoice === SCISSORS) ||
+        (cChoice === SCISSORS && pChoice === ROCK)
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
+startGameBtn.addEventListener("click", function () {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log("Game is starting...");
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+    const winner = getWinner(computerChoice, playerChoice);
+
+    console.log(winner);
+});

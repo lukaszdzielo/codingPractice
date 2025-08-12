13bfdb0565b659380c599b2c9d96503cd1a56e59
Lukasz Dzielo <lukaszdzielo.dev@gmail.com>
2025-08-12 13:02:58 +0200

    ex6 update js mini shop

--- javascript-the-complete-guide-2020-beginner-advanced/06-miniShop/assets/scripts/app.js
+++ javascript-the-complete-guide-2020-beginner-advanced/06-miniShop/assets/scripts/app.js
@@ -1,39 +1,69 @@
 class Product {
-	constructor(title, image, price, desc) {
-		this.title = title ?? 'Default';
-		this.imgUrl = image;
-		this.description = desc;
-		this.price = price;
-	}
+	// title = "DEFAULT";
+	// imagrUrl;
+	// description;
+	// price;
+   constructor(title, image, price, desc) {
+      this.title = title;
+      this.imgUrl = image;
+      this.description = desc;
+      this.price = price;
+   }
 }
 
-const productList = {
-	products: [
-		new Product('A Pillow', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Pillows_on_a_hotel_bed.jpg/1920px-Pillows_on_a_hotel_bed.jpg', 19.99, 'A soft pillow!'),
-		new Product('A Carpet', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg', 89.99, 'A carpet which you might like - or not.'),
-	],
+class ProductItem {
+   constructor(product) {
+      this.product = product;
+   }
+
+   render () {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+         <div>
+            <img src="${this.product.imgUrl}" alt="${this.product.title}">
+            <div class="product-item__content">
+               <h2>${this.product.title}</h2>
+               <h3>\$${this.product.price}</h3>
+               <p>${this.product.description}</p>
+               <button>Add to Cart</button>
+            </div>
+         </div>
+      `;
+		return prodEl;
+   }
+}
+
+class ProductList {
+	products = [
+		new Product(
+         "A Pillow",
+         "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Pillows_on_a_hotel_bed.jpg/1920px-Pillows_on_a_hotel_bed.jpg",
+         19.99,
+         "A soft pillow!"
+      ),
+      new Product(
+         "A Carpet",
+         "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+         89.99,
+         "A carpet which you might like - or not."
+      ),
+	];
+
+	constructor() {}
+
 	render() {
-		const renderHook = document.getElementById('app');
-		const prodList = document.createElement('ul');
-		prodList.className = 'product-list';
-		for ( const prod of this.products) {
-			const prodEl = document.createElement('li');
-			prodEl.className = 'product-item';
-			prodEl.innerHTML = `
-				<div>
-					<img src="${prod.imgUrl}" alt="${prod.title}">
-					<div class="product-item__content">
-						<h2>${prod.title}</h2>
-						<h3>\$${prod.price}</h3>
-						<p>${prod.description}</p>
-						<button>Add to Cart</button>
-					</div>
-				</div>
-			`;
-			prodList.append(prodEl);
-		}
-		renderHook.append(prodList);
-	}
+      const renderHook = document.getElementById("app");
+      const prodList = document.createElement("ul");
+      prodList.className = "product-list";
+      for (const prod of this.products) {
+         const prodItem = new ProductItem(prod);
+			const prodEl = prodItem.render();
+         prodList.append(prodEl);
+      }
+      renderHook.append(prodList);
+   }
 }
 
+const productList = new ProductList();
 productList.render();

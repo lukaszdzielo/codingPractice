d3749b6abc66551259188c2678b96dc3f916907e
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-10-14 21:41:56 +0200

    vue watch, computed, methods

--- vuejs-2-the-complete-guide/05-using-the-native-event-object/app.js
+++ vuejs-2-the-complete-guide/05-using-the-native-event-object/app.js
@@ -1,25 +1,57 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: 'Dan'
-    };
+	data() {
+		return {
+			counter: 0,
+			name: "",
+      lastName: '',
+      // fullName: '',
+		};
+	},
+  watch: {
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = ''
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName
+    //   };
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = ''
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   };
+    // }
   },
-  methods: {
-    setName(event, lastName) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput() {
-      this.name = '';
-    }
-  }
+	computed: {
+		fullName() {
+			console.log("Running again...");
+			if (this.name === "" || this.lastName === "") {
+        return ""
+      };
+			return this.name + " " + this.lastName;
+		},
+	},
+	methods: {
+		outputFullName() {
+			console.log("Running again...");
+			if (this.name === "") return "";
+			return this.name + " " + "Schwarzmüller";
+		},
+		setName(event) {
+			this.name = event.target.value;
+		},
+		add(num) {
+			this.counter = this.counter + num;
+		},
+		reduce(num) {
+			this.counter = this.counter - num;
+			// this.counter--;
+		},
+		resetInput() {
+			this.name = "";
+		},
+	},
 });
 
-app.mount('#events');
+app.mount("#events");

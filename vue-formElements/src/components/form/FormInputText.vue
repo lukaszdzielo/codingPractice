d22b9882e9bb90f1cd5c09d6acc96f9f89d9163e
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-10-20 20:48:44 +0200

    vue form components test

---
+++ vue-formElements/src/components/form/FormInputText.vue
@@ -0,0 +1,14 @@
+<script setup>
+    defineProps({
+        elementClass: {
+            type: String,
+            default: 'w-full rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6',
+        },
+        modelValue: String,
+    })
+    defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <input type="text" :class="elementClass" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+</template>
---
+++ vue-formElements/src/components/form/FormLabel.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <label>
+        <slot />
+    </label>
+</template>

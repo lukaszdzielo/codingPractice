0cd01a453099818b5fb8fe45db98d6cb05b6f1c4
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-10-30 19:36:41 +0100

    vue 07

---
+++ vuejs-2-the-complete-guide/07-vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-show="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '1234 6578 991',
+                email: 'manuel@localhost.com',
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+</script>

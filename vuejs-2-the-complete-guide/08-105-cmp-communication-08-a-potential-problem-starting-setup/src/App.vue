8dab47348b266405f1c68d513e81f27691dda29a
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-11-13 19:29:11 +0100

    vue 09 init

--- vuejs-2-the-complete-guide/08-105-cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
+++ vuejs-2-the-complete-guide/08-105-cmp-communication-08-a-potential-problem-starting-setup/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base />
   </div>
 </template>
 
@@ -34,6 +34,7 @@ export default {
   provide() {
     return {
       topics: this.topics,
+      selectTopic: this.activateTopic,
     }
   },
   methods: {

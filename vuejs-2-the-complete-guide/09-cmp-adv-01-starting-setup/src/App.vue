f005281c14054121c6b4cf29f412a5e839366999
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-11-13 19:46:07 +0100

    vue 09 scoped

--- vuejs-2-the-complete-guide/09-cmp-adv-01-starting-setup/src/App.vue
+++ vuejs-2-the-complete-guide/09-cmp-adv-01-starting-setup/src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
+    
+    <the-header />
+    
+    <badge-list />
+    
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
+
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 export default {
+  components: {
+    'the-header': TheHeader,
+    // TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
@@ -24,7 +37,7 @@ export default {
 };
 </script>
 
-<style>
+<style >
 html {
   font-family: sans-serif;
 }

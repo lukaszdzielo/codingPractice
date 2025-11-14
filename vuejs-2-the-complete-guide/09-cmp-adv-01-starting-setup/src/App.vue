886611be46edf13d653131fa57ca37270d057fbc
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-11-14 20:46:15 +0100

    vue 09 slots

--- vuejs-2-the-complete-guide/09-cmp-adv-01-starting-setup/src/App.vue
+++ vuejs-2-the-complete-guide/09-cmp-adv-01-starting-setup/src/App.vue
@@ -11,6 +11,11 @@
       :role="activeUser.role"
     />
 
+    <course-goals #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps["anotherProp"] }}</p>
+    </course-goals>
+
   </div>
 </template>
 
@@ -18,12 +23,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 export default {
   components: {
     'the-header': TheHeader,
     // TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {

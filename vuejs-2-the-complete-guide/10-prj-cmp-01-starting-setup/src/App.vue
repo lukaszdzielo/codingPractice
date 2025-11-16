4b08be573c478edb471888667f61dbb7e1f7a6b9
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-11-16 19:22:02 +0100

    vue 10 init

---
+++ vuejs-2-the-complete-guide/10-prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <learning-resource v-for="res in storeResources" :key="res.id"></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org/',
+        },
+      ],
+    }
+  },
+}
+</script>

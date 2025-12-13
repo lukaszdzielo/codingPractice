341367fa7202112221dc56d94531d94149b2a027
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-12-13 09:42:21 +0100

    vue 14  transition

---
+++ vuejs-2-the-complete-guide/14-animations-transitions/src/pages/UsersList.vue
@@ -0,0 +1,61 @@
+<template>
+  <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save changes</button>
+  <ul>
+    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from '../components/users/UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
+  methods: {
+    confirmInput() {
+      // this.$router.push('/users/some-user-id');
+      this.$router.push('/teams');
+      // this.$router.back();
+      // this.$router.forward();
+    },
+    saveChanges() {
+      this.changesSaved = true;
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('UsersList Cmp beforeRouteEnter');
+    console.log(to, from);
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList Cmp beforeRouteLeave');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved changes!');
+      next(userWantsToLeave);
+    }
+  },
+  unmounted() {
+    console.log('unmounted');
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>

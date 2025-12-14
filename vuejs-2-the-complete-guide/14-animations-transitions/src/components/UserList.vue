ba91073b31fc8a1d9a7b98e2234ebe40e720202b
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-12-14 01:29:55 +0100

    vue 14  transition

--- vuejs-2-the-complete-guide/14-animations-transitions/src/components/UserList.vue
+++ vuejs-2-the-complete-guide/14-animations-transitions/src/components/UserList.vue
@@ -1,7 +1,7 @@
 <template>
-    <ul>
+    <transition-group tag="ul" name="user-list">
         <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-    </ul>
+    </transition-group>
     <div>
         <input type="text" ref="userNameInput">
         <button @click="addUser()">Add User</button>
@@ -38,5 +38,35 @@
         padding: 1rem;
         border: 1px solid #ccc;
         text-align: center;
+    }
+
+    .user-list-enter-from {
+        opacity: 0;
+        transform: translateX(-30px)
+    }
+
+    .user-list-enter-to,
+    .user-list-leave-from {
+        opacity: 1;
+        transform: translateX(0)
+        
+    }
+
+    .user-list-enter-active {
+        transition: all 0.3s ease-out;
+    }
+
+    .user-list-leave-active {
+        position: absolute;
+        transition: all 0.3s ease-in;
+    }
+
+    .user-list-leave-to {
+        opacity: 0;
+        transform: translateX(30px);
+    }
+
+    .user-list-move {
+        transition: all 0.8s ease;
     }
 </style>

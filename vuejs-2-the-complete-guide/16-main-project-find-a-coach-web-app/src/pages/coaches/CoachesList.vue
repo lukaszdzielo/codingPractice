7f40d203c5be6c5912aa88bf3c40c4e78b20031d
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-12-26 00:23:50 +0100

    vue 16 main project

--- vuejs-2-the-complete-guide/16-main-project-find-a-coach-web-app/src/pages/coaches/CoachesList.vue
+++ vuejs-2-the-complete-guide/16-main-project-find-a-coach-web-app/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,9 @@
 <template>
 
+    <BaseDialog :show="!!error" title="An Error Occurred" @close="handleError">
+        <p>{{ error }}</p>
+    </BaseDialog>
+
     <section>
         <CoachFilter @change-filter="setFilter" />
     </section>
@@ -7,10 +11,13 @@
     <section>
         <BaseCard>
             <div class="controls">
-                <BaseButton mode="outline">Refresh</BaseButton>
-                <BaseButton v-if="!isCoach" link to="/register">Register as coach</BaseButton>
+                <BaseButton mode="outline" @click="loadCoaches(true)">Refresh</BaseButton>
+                <BaseButton v-if="!isCoach && !isLoading" link to="/register">Register as coach</BaseButton>
             </div>
-            <ul v-if="hasCoaches">
+            <div v-if="isLoading">
+                <BaseSpinner></BaseSpinner>
+            </div>
+            <ul v-else-if="hasCoaches">
                 <CoachItem v-for="coach in filteredCoaches" :key="coach.id"
                     :id="coach.id"
                     :first-name="coach.firstName"
@@ -36,6 +43,8 @@ import CoachFilter from '@/components/coaches/CoachFilter.vue';
         },
         data() {
             return {
+                isLoading: false,
+                error: null,
                 activeFilters: {
                     frontend: true,
                     backend: true,
@@ -61,12 +70,27 @@ import CoachFilter from '@/components/coaches/CoachFilter.vue';
                 });
             },
             hasCoaches() {
-                return this.$store.getters['coaches/coaches'];
+                return !this.isLoading && this.$store.getters['coaches/coaches'];
             }   
+        },
+        created() {
+            this.loadCoaches();
         },
         methods: {
             setFilter(updatedFilters) {
                 this.activeFilters = updatedFilters;
+            },
+            async loadCoaches(refresh = false) {
+                this.isLoading = true;
+                try {
+                    await this.$store.dispatch('coaches/loadCoaches', { forceRefresh: refresh });
+                } catch (error) {
+                    this.error = error.message || 'Something went wrong!';
+                }
+                this.isLoading = false;
+            },
+            handleError() {
+                this.error = null;
             },
         },
     }
--- vuejs-2-the-complete-guide/16-main-project-find-a-coach-web-app/src/pages/requests/RequestsReceived.vue
+++ vuejs-2-the-complete-guide/16-main-project-find-a-coach-web-app/src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+	<BaseDialog :show="!!error" title="An Error Occurred" @close="handleError">
+		<p>{{ error }}</p>
+	</BaseDialog>
 	<section>
 		<base-card>
 			<header>
 				<h2>Requests Received</h2>
 			</header>
-			<ul v-if="hasRequests">
+			<BaseSpinner v-if="isLoading"></BaseSpinner>
+			<ul v-else-if="hasRequests && !isLoading">
 				<RequestItem v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message">
 				</RequestItem>
 			</ul>
@@ -15,14 +19,39 @@
 
 <script>
 import RequestItem from "@/components/requests/RequestItem.vue";
+import BaseDialog from "@/components/ui/BaseDialog.vue";
+import BaseSpinner from "@/components/ui/BaseSpinner.vue";
 export default {
 	components: { RequestItem },
+	data() {
+		return {
+			isLoading: false,
+			error: null,
+		};
+	},
 	computed: {
 		receivedRequests() {
 			return this.$store.getters['requests/requests'];
 		},
 		hasRequests() {
 			return this.$store.getters['requests/hasRequests'];
+		},
+	},
+	created() {
+		this.loadRequests();
+	},
+	methods: {
+		async loadRequests() {
+			this.isLoading = true;
+			try {
+				await this.$store.dispatch('requests/fetchRequests');
+			} catch (error) {
+				this.error = error.message || 'Something failed!';
+			}
+			this.isLoading = false;
+		},
+		handleError() {
+			this.error = null;
 		},
 	},
 };

72995034a561e99b198e8beaf1c32f48ef8821d8
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-12-27 17:09:52 +0100

    vue 17 auth

--- vuejs-2-the-complete-guide/17-authentication/src/store/modules/auth/actions.js
+++ vuejs-2-the-complete-guide/17-authentication/src/store/modules/auth/actions.js
@@ -1,6 +1,26 @@
 export default {
     async login(context, payload) {
-        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD7xuKbDurQJsJHg1K00qzMFejiT_7f8ag`, {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login',
+        })
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup',
+        })
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        const apiKey = 'AIzaSyD7xuKbDurQJsJHg1K00qzMFejiT_7f8ag';
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
+
+        if (mode === 'signup') {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`
+        }
+
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -10,39 +30,30 @@ export default {
         })
         const responseData = await response.json();
         if (!response.ok) {
-            console.log(responseData)
             const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
             throw error;
         }
+
+        localStorage.setItem('token', responseData.idToken)
+        localStorage.setItem('userId', responseData.localId)
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn,
         });
     },
-    async signup(context, payload) {
-        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD7xuKbDurQJsJHg1K00qzMFejiT_7f8ag`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true,
-            })
-        })
-        const responseData = await response.json();
-        if (!response.ok) {
-            console.log(responseData)
-            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
-            throw error;
+    tryLogin(context) {
+        const token = localStorage.getItem('token')
+        const userId = localStorage.getItem('userId')
+
+        if (token && userId) {
+            context.commit('setUser', {
+                token,
+                userId,
+                tokenExpiration: null,
+            });
         }
-
-        console.log(responseData);
-
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn,
-        });
     },
     logout(context) {
         context.commit('setUser', {

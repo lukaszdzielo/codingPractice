97a403a940df2628bb2ee617455e17ba4a04dacb
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2026-01-03 11:29:25 +0100

    vue 19 composition

--- vuejs-2-the-complete-guide/19-composition-api/src/App.vue
+++ vuejs-2-the-complete-guide/19-composition-api/src/App.vue
@@ -2,26 +2,37 @@
   <section class="container">
     <!-- <h2>{{ userName }}</h2>
     <h3>{{ userAge }}</h3> -->
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ user3.name }}</h2>
+    <h3>{{ user3.age }}</h3>
   </section>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, isRef, reactive, isReactive, toRefs } from 'vue';
  
 // const userName = ref('Maximilian');
-// const userAge = ref(11);
 
-// const user = ref({
-//   name: 'Maximilian',
-//   age: 22,
-// });
+const userAge = ref(11);
 
-const user = reactive({
+const user2 = ref({
   name: 'Maximilian',
   age: 22,
 });
+
+const user3 = reactive({
+  name: 'Maximilian',
+  age: 22,
+});
+
+console.log( '1', isRef(userAge) ) // true
+console.log( '2', isRef(user2) ) // true
+console.log( '3', isReactive(user3) ) // true
+console.log( '4', isReactive(user3.name) ) // false
+
+const userRefs = toRefs(user3)
+
+console.log( '5', isReactive(userRefs) ) // false
+console.log( '6', isReactive(userRefs.name) ) // false
 
 setTimeout(function() {
   // userName.value = 'Max';
@@ -32,8 +43,8 @@ setTimeout(function() {
   // user.value.age = 22;
 
   // reactive
-  user.name = 'Max';
-  user.age = 22;
+  user3.name = 'Max';
+  user3.age = 22;
 }, 2000);
 </script>
 

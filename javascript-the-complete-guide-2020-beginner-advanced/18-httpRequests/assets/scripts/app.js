1adf662e2fd849cfadf192b2980d9e79e4f97e00
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2026-01-31 10:04:50 +0100

    js requests

--- javascript-the-complete-guide-2020-beginner-advanced/18-httpRequests/assets/scripts/app.js
+++ javascript-the-complete-guide-2020-beginner-advanced/18-httpRequests/assets/scripts/app.js
@@ -1,16 +1,23 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
-// xhr.onload = function() {
-//     listofPosts = JSON.parse(xhr.response);
-//     console.log(listofPosts);
-// }
 
-xhr.responseType = 'json';
+function sendHttpRequest(method, url) {
 
-xhr.onload = function() {
-    listofPosts = xhr.response;
-    console.log(listofPosts);
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.send();
+    })
+    return promise;
+}
+
+async function fetchPosts() {
+    const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
+    const listofPosts = responseData;
     for (const post of listofPosts) {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
@@ -19,7 +26,8 @@ xhr.onload = function() {
     }
 }
 
-xhr.send();
+fetchPosts();
+
 
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');

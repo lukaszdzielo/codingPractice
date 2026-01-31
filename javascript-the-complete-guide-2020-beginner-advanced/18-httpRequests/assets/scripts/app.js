8a1b26cc15dc13948dd12d9da80104a5d977ed2a
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2026-01-31 10:13:51 +0100

    js requests

--- javascript-the-complete-guide-2020-beginner-advanced/18-httpRequests/assets/scripts/app.js
+++ javascript-the-complete-guide-2020-beginner-advanced/18-httpRequests/assets/scripts/app.js
@@ -1,4 +1,7 @@
-
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
 
 function sendHttpRequest(method, url, data) {
 
@@ -36,8 +39,11 @@ async function createPost(title, content) {
     sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
-fetchPosts();
-createPost('A New Post', 'This is the content of the new post!');
-
-const listElement = document.querySelector('.posts');
-const postTemplate = document.getElementById('single-post');
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector('#title').value;
+    const enteredContent = event.currentTarget.querySelector('#content').value;
+    createPost(enteredTitle, enteredContent);
+});
+// createPost('A New Post', 'This is the content of the new post!');

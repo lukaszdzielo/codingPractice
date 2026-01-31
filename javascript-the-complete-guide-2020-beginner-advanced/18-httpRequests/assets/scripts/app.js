f3f0d48e95ea6bdd394350b657dd322aa11feae9
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2026-01-31 13:22:42 +0100

    js requests

--- javascript-the-complete-guide-2020-beginner-advanced/18-httpRequests/assets/scripts/app.js
+++ javascript-the-complete-guide-2020-beginner-advanced/18-httpRequests/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
 
@@ -25,6 +26,7 @@ async function fetchPosts() {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body;
+        postEl.querySelector('li').id = post.id;
         listElement.append(postEl);
     }
 }
@@ -46,4 +48,10 @@ form.addEventListener('submit', event => {
     const enteredContent = event.currentTarget.querySelector('#content').value;
     createPost(enteredTitle, enteredContent);
 });
-// createPost('A New Post', 'This is the content of the new post!');
+
+postList.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        const postId = event.target.closest('li').id;
+        sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
+    }
+})
